b9aba00a439efb512d31d8953c3c19288b237259
Nili Stein <nilistein@campus.technion.ac.il>
2018-06-19 18:09:54 +0300

    insmod works
    no seg faults on init

--- intercept.c
+++ intercept.c
@@ -1,3 +1,5 @@
+#include <linux/sched.h>
+#include <stdbool.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/utsname.h>
@@ -14,32 +16,38 @@ void **sys_call_table = NULL;
 asmlinkage long (*original_sys_kill)(void);
 
 char *program_name = NULL;
+int scan_range = 0;
 MODULE_PARM(program_name, "s");
+MODULE_PARM(scan_range, "i");
 
-bool compare_name(char *str1, char *str2) {
-    for (int i = 0; i < MAX_NAME_SIZE; i++) {
+bool compare_names(char *str1, char *str2) {
+    int i;
+    for (i = 0; (i < MAX_NAME_SIZE) && str1 && str2; i++) {
         if (str1[i] != str2[i])
             return false;
     }
+    if(str1[i] != '\0' || str2[i] != '\0') return false;
     return true;
 }
 
 /* New fake syscall: */
 asmlinkage long our_sys_kill(int pid, int sig) {
-    if (compare_names(find_task_by_pid(pid)->comm, program_name) &&
+    task_t* p = find_task_by_pid(pid);
+    if(!pid) {
+        return sys_kill(pid, sig);
+    }
+    if (compare_names(p->comm, program_name) &&
         sig == SIGKILL) {
         return -EPERM;
     }
-    return sys_kill();
+    return sys_kill(pid, sig);
 }
 
-// TODO: import original syscall and write new syscall
-
 void find_sys_call_table(int scan_range) {
-    int *p = system_utsname;
+    void *p = &system_utsname;
     int i = 0;
-    for (; i < scan_range; p++, i++) {
-        if (p == sys_read)
+    for (; i < scan_range; p += sizeof(void*), i++) {
+        if (p == &sys_read)
             break;
     }
     p = p - 3; //sys_read syscall num is 3, so reduce the offset to get to syscall table
@@ -48,6 +56,7 @@ void find_sys_call_table(int scan_range) {
 
 int init_module(void) {
     /* set sys_kill syscall to our fake syscall: */
+    find_sys_call_table(scan_range);
     original_sys_kill = sys_call_table[37]; //sys_kill num is 37
     sys_call_table[37] = our_sys_kill;
     return 0;
